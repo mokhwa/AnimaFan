35ca30882f92db049a00a3d3c612c0d39339a3e9
D7703_22 <d7703_22@promise.local>
2019-05-09 16:30:14 +0900

    Initial Commit

---
+++ gg/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gg
+//
+//  Created by D7703_22 on 2019. 5. 9..
+//  Copyright © 2019년 fb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
